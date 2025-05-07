043f16317e3a52b3ac1060d713e97835afc4015c
Golchha-Harsh <harsh.golchha@systango.com>
2025-05-07 15:33:44 +0530

    Seprate Code use server in seprate file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,33 +1,21 @@
+import { shareMeal } from "@/lib/actions";
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 export default function ShareMealPage() {
   //When the user submits the form (by clicking the "Share Meal" button), Next.js intercepts the form submission
   //and sends the form data to the shareMeal function on the server.The browser does not perform a full page reload.
-  async function shareMeal(formData) {
-    //In Next.js, the "use server" directive allows you to define functions that execute on the server, even when they're written within a component file.
-    //This is a powerful feature that enables you to handle form submissions, database interactions, and other server-side logic directly within your Next.js application, rather than needing a separate API.
-    //Server-Side Logic: Inside the shareMeal function, you can now perform any server-side operations you need, such as:
-
-    //Validating the form data.
-
-    //Saving the data to a database (as you would do to save the meal).
-
-    //Sending emails.
-
-    //Interacting with other APIs.
-
-    //Generating a response (e.g., redirecting the user to a success page).
-    "use server";
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-    console.log(meal);
-  }
+  // async function shareMeal(formData) {
+  //   "use server";
+  //   const meal = {
+  //     title: formData.get("title"),
+  //     summary: formData.get("summary"),
+  //     instructions: formData.get("instructions"),
+  //     image: formData.get("image"),
+  //     creator: formData.get("name"),
+  //     creator_email: formData.get("email"),
+  //   };
+  //   console.log(meal);
+  // }
   return (
     <>
       <header className={classes.header}>
