90f3acc7298c3d6b67d06188a6d4173552bb89c9
Golchha-Harsh <harsh.golchha@systango.com>
2025-05-07 12:14:44 +0530

    Throwing Not Found Errors for Individual Meals

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDefaultPage({ params }) {
   const meal = getMeal(params.mealslug);
+  if(!meal)
+  {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
---
+++ app/meals/not-found.js
@@ -0,0 +1,4 @@
+export default function NotFound(){
+    return <main className="notfound"><h1>Meal Not Found!!!</h1>
+    <p>Unfortunately, we could not find the requested page or meal data.</p></main>
+}
